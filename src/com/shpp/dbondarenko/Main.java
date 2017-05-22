12a63b1d56d7514c710ccbe10b9916aa090f6380
Dmitro Bondarenko <bondes87@gmail.com>
2017-05-22 23:15:03 +0300

    Initial commit

---
+++ src/com/shpp/dbondarenko/Main.java
@@ -0,0 +1,11 @@
+package com.shpp.dbondarenko;
+
+/**
+ * File: Main.java
+ * Created by Dmitro Bondarenko on 22.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
