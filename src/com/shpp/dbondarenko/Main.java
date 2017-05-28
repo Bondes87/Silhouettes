b1765b375f2d1ee0a7bdd48968ad6bf4dce38394
Dmitro Bondarenko <bondes87@gmail.com>
2017-05-28 19:27:36 +0300

    Implemented a check for the presence of the path to the image.

--- src/com/shpp/dbondarenko/Main.java
+++ src/com/shpp/dbondarenko/Main.java
@@ -10,9 +10,15 @@ public class Main {
     public static void main(String[] args) {
         Recognizer recognizer = new Recognizer();
         try {
-            System.out.println(recognizer.recognize("man.jpg"));
+            if (args.length == 0) {
+                throw new Exception("No path to file specified.");
+            } else {
+                System.out.println(recognizer.recognize(args[0]));
+            }
         } catch (IOException e) {
             System.out.println("Image not found.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
