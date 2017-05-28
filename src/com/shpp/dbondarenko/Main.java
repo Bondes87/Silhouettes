ef6434170ea0ceeb43a5321c18b1416608033146
Dmitro Bondarenko <bondes87@gmail.com>
2017-05-28 17:00:11 +0300

    Сreated a method of calculating the area of the silhouette (used the algorithm in width).

--- src/com/shpp/dbondarenko/Main.java
+++ src/com/shpp/dbondarenko/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Recognizer recognizer = new Recognizer();
         try {
-            recognizer.recognize("man.jpg");
+            recognizer.recognize("man2.jpg");
         } catch (IOException e) {
             System.out.println("Image not found.");
         }
--- src/com/shpp/dbondarenko/Recognizer.java
+++ src/com/shpp/dbondarenko/Recognizer.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  * File: Recognizer.java
@@ -13,6 +15,8 @@ import java.io.IOException;
 public class Recognizer {
     public int recognize(String imagePath) throws IOException {
         int[][] pixelsColor = readFile(imagePath);
+        System.out.println(pixelsColor.length + " " + pixelsColor[0].length);
+        ArrayList<Integer> silhouettes = searchSilhouettes(pixelsColor);
         return 0;
     }
 
@@ -20,12 +24,62 @@ public class Recognizer {
         BufferedImage image = ImageIO.read(new File(imagePath));
         int imageWidth = image.getWidth();
         int imageHeight = image.getHeight();
-        int[][] pixelsColor = new int[imageWidth][imageHeight];
-        for (int i = 0; i < imageWidth; i++)
-            for (int j = 0; j < imageHeight; j++) {
-                Color color = new Color(image.getRGB(i, j));
-                pixelsColor[i][j] = color.getRed();
+        int[][] pixelsColor = new int[imageHeight][imageWidth];
+        for (int i = 0; i < imageHeight; i++) {
+            for (int j = 0; j < imageWidth; j++) {
+                Color color = new Color(image.getRGB(j, i));
+                pixelsColor[i][j] = color.getRed() <= 127 ? 0 : 255;
+                //System.out.println(pixelsColor[i][j]);
             }
+        }
         return pixelsColor;
+    }
+
+    private ArrayList<Integer> searchSilhouettes(int[][] pixelsColor) {
+        ArrayList<Integer> silhouettes = new ArrayList<>();
+        //int attemptsNumber = new Random().nextInt(pixelsColor.length);
+        int row = 100;
+        for (int i = 1; i < pixelsColor[row].length; i++) {
+            if (pixelsColor[row][i] != pixelsColor[row][i - 1]) {
+                silhouettes.add(calculateSilhouetteArea(pixelsColor, row, i));
+            }
+        }
+        System.out.println(silhouettes);
+        return null;
+    }
+
+    private int calculateSilhouetteArea(int[][] pixelsColor, int row, int col) {
+        int silhouetteArea = 0;
+        ArrayList<int[]> motionCoordinates = new ArrayList<>();
+        silhouetteArea++;
+        motionCoordinates.add(new int[]{row, col});
+        pixelsColor[row][col] = pixelsColor[row][col - 1];
+        while (!motionCoordinates.isEmpty()) {
+            int y = motionCoordinates.get(0)[0];
+            int x = motionCoordinates.get(0)[1];
+            if (y != 0 && pixelsColor[y][x] != pixelsColor[y - 1][x]) {
+                silhouetteArea++;
+                motionCoordinates.add(new int[]{y - 1, x});
+                pixelsColor[y - 1][x] = pixelsColor[y][x];
+            }
+            if (x != pixelsColor[0].length - 1 && pixelsColor[y][x] != pixelsColor[y][x + 1]) {
+                silhouetteArea++;
+                motionCoordinates.add(new int[]{y, x + 1});
+                pixelsColor[y][x + 1] = pixelsColor[y][x];
+            }
+            if (y != pixelsColor.length - 1 && pixelsColor[y][x] != pixelsColor[y + 1][x]) {
+                silhouetteArea++;
+                motionCoordinates.add(new int[]{y + 1, x});
+                pixelsColor[y + 1][x] = pixelsColor[y][x];
+            }
+            if (x != 0 && pixelsColor[y][x] != pixelsColor[y][x - 1]) {
+                silhouetteArea++;
+                motionCoordinates.add(new int[]{y, x - 1});
+                pixelsColor[y][x - 1] = pixelsColor[y][x];
+            }
+            motionCoordinates.remove(0);
+        }
+        System.out.println(silhouetteArea);
+        return silhouetteArea;
     }
 }
